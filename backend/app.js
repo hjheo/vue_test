47892b28c35d38dabf2442977885c51c9e501c04
hjheo <hhj6409@hanmail.net>
2020-05-24 17:27:44 +0900

    app.js 수정

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var history = require('connect-history-api-fallback');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var workRouter = require('./routes/works');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(history());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/works', workRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
