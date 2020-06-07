c43779960642f2a473ae4f087b6537d7e088b9aa
hjheo <hhj6409@hanmail.net>
2020-06-07 18:48:55 +0900

    ModalCountsComponent 코드 정리

--- frontend/src/components/ModalCountsComponent.vue
+++ frontend/src/components/ModalCountsComponent.vue
@@ -1,38 +1,36 @@
 <template lang="html">
   <table class="ui small single line structured very compact very basic table">
     <thead class="center aligned">
-      <tr>
-        <th v-for="key in countListKey">{{ key }}</th>
-        <th><div class="ui small transparent input" v-for="count in countChild">
+      <tr><th v-for="key in countListKey">{{ key }}</th>
+      <th>
+        <div class="ui small transparent input" v-for="count in countChild">
           <input type="text" v-model="count.filter" />
-        </div></th>
-      </tr>
+        </div>
+      </th></tr>
     </thead>
     <tbody class="center aligned">
-      <tr>
-        <td v-for="value in countListValue">
-          <input type="text" v-model="params[value]" @focus="selectTarget($event)" />
-        </td>
-        <td class="two wide">
+      <tr><td v-for="value in countListValue">
+        <input type="text" v-model="params[value]" @focus="selectTarget($event)" />
+      </td>
+      <td class="two wide">
+        <div class="ui action input">
+          <input type="text" v-model="paramsChild.count" @focus="selectTarget($event)" />
+          <button class="ui icon button" @click="updateCount(paramsChild.count)">
+            <i class="save icon"></i>
+          </button>
+        </div>
+      </td>
+      <td class="four wide" id="countTd">
+        <span v-for="count in countChild">
+          <input type="text" class="defaultQty" v-model="count.defaultQty" @focus="selectTarget($event)" />
           <div class="ui action input">
-            <input type="text" v-model="paramsChild.count" @focus="selectTarget($event)" />
-            <button class="ui icon button" @click="updateCount(paramsChild.count)">
+            <input type="text" v-model="count.overQty" @focus="selectTarget($event)" />
+            <button class="ui icon button" @click="updateQty(count)">
               <i class="save icon"></i>
             </button>
           </div>
-        </td>
-        <td class="four wide" id="countTd">
-          <span v-for="count in countChild">
-            <input type="text" class="defaultQty" v-model="count.defaultQty" @focus="selectTarget($event)" />
-            <div class="ui action input">
-              <input type="text" v-model="count.overQty" @focus="selectTarget($event)" />
-              <button class="ui icon button" @click="updateQty(count)">
-                <i class="save icon"></i>
-              </button>
-            </div>
-          </span>
-        </td>
-      </tr>
+        </span>
+      </td></tr>
     </tbody>
   </table>
 </template>
@@ -52,7 +50,7 @@ export default {
   },
   methods: {
     selectTarget(event) {
-      event.target.select();
+      event.target.select()
     },
     updateCount(count) {
       this.$emit('update-count', count)
